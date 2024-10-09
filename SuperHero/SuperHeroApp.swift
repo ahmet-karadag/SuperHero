681a2016d60a338b0da8a457f769d42cf3df9894
Ahmet Karadağ <akkaradaga@gmail.com>
2024-10-09 16:48:53 +0300

    Initial Commit

---
+++ SuperHero/SuperHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp.swift
+//  SuperHero
+//
+//  Created by ahmet karadağ on 9.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
